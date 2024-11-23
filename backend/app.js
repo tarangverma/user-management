55b581cc7dbdd2e146765cf20a3f38ec380ca331
tarangverma <tarangverma004@gmail.com>
2024-11-24 01:27:26 +0530

    fix: deployment fix

--- backend/app.js
+++ backend/app.js
@@ -10,5 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
