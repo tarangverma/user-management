be8a4faf7613dc0d2957cb541e1fe059b1aca76b
tarangverma <tarangverma004@gmail.com>
2024-11-24 13:45:28 +0530

    feat: deploy done

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = process.env.VUE_APP_API_URL;
 
 export const loginUser = async (email, password) => {
   const { data } = await axios.post(`${API_URL}/login`, { email, password });
